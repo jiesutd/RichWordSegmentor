54fd0353e4bef16ffc7ab2ac795a240975f2d232
Jiesutd <jie_yang@mymail.sutd.edu.sg>
2017-07-20 14:09:16 +0800

    update demo

--- demo.sh
+++ demo.sh
@@ -1,5 +1,5 @@
 sh cleanall.sh
 cmake .
 make STDSeg
-./STDSeg -l -train ../example/train.debug -dev ../example/dev.debug -test ../example/test.debug -option ../example/option.STD -model ../example/debug.model -word ../example/ctb.50d.word.debug
-./STDSeg -test ../example/test.debug -model ../example/debug.model -output ../example/test.debug.out
+./STDSeg -l -train ./example/train.debug -dev ./example/dev.debug -test ./example/test.debug -option ./example/option.STD -model ./example/debug.model -word ./example/ctb.50d.word.debug
+./STDSeg -test ./example/test.debug -model ./example/debug.model -output ./example/test.debug.out
